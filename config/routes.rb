33016ce54d1d55f1a7846b97ddecdffa8998dabb
nachosala89 <nachosala89@gmail.com>
2022-02-15 08:25:10 -0300

    Display Greeting component

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "static#index"
   get '/messages/', to: 'messages#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
